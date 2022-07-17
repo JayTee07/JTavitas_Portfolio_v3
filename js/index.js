e0adcdda4cd26bc4201731e61af0dd97941a9d8d
JayTee07 <jaytavitas81@gmail.com>
2022-07-17 04:40:36 -0500

    Responsive Adjustments
    made some adjustments to the media queries and list in the  html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
+$(".skillText").click(function(){
+    $(this).find(".skillList_grid").toggleClass("active");
+    $(this).siblings(".skillTitle").find(".skillList_grid").removeClass("active");
+    if ($(".skillList_grid").hasClass("active")) {
+        $(".plusIcon").removeClass("fa-plus").addClass("fa-minus");
+    }
+    else {
+        $(".plusIcon").removeClass("fa-minus").addClass("fa-plus");
+        }
+});
+
+$(".appText").click(function(){
+    $(this).find(".appList_grid").toggleClass("app-active");
+    $(this).siblings(".appTitle").find(".appList_grid").removeClass("app-active");
+    if ($(".appList_grid").hasClass("app-active")) {
+        $(".app-plusIcon").removeClass("fa-plus").addClass("fa-minus");
+    }
+    else {
+        $(".app-plusIcon").removeClass("fa-minus").addClass("fa-plus");
+        }
+});
+
+$(".intText").click(function(){
+    $(this).find(".intList_grid").toggleClass("int-active");
+    $(this).siblings(".intTitle").find(".intList_grid").removeClass("int-active");
+    if ($(".intList_grid").hasClass("int-active")) {
+        $(".int-plusIcon").removeClass("fa-plus").addClass("fa-minus");
+    }
+    else {
+        $(".int-plusIcon").removeClass("fa-minus").addClass("fa-plus");
+        }
+});
+
 /* Hamburger to X Animation */
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
