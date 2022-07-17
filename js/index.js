e510937ea19010b59da430e434447aa91d805863
JayTee07 <jaytavitas81@gmail.com>
2022-07-16 23:12:03 -0500

    Updated CSS
    updated css for responsiveness.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 /* Hamburger to X Animation */
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
+const header = document.querySelector("header");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    header.classList.toggle("active");
 });
 
 document.querySelectorAll(".navMenu", "li").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    header.classList.remove("active");
 }));
